4da2b163194fd1ceaa2444ea68d0c3fa5f9baf56
Fernando Romero <romerocruzfernando@gmail.com>
2024-07-20 12:18:21 -0600

    Recursos del tablero documentados

--- Recursos/Tablero.cs
+++ Recursos/Tablero.cs
@@ -1,10 +1,20 @@
 using System;
 
+/*
+ * ******************************* OODA CHESS **************************************
+ *
+ * Autores:
+ * - dannaliz
+ * - Ookami5018
+ *
+ * En este archivo definimos una parte de los recursos que utlizaremos para Ooda Chess
+ * Particularmente son aquellos que tienen que ver con el Tablero
+ */
 namespace Recursos
 {
 
     /*
-     * Clase coordenada que representa las coordenadas tipicas de la notacion del ajedrez
+     * Clase Coordenada que modela las coordenadas tipicas de la notacion del ajedrez
      * Ejemplo (A4,B8,...)
      */
     public class Coordenada
@@ -41,12 +51,14 @@ namespace Recursos
                     _fila = value;
                 }
                 else{
-                    throw new System.InvalidOperationException();
+                    throw new InvalidOperationException();
                 }
             }
         }
 
-        // Propiedad que representa la columna del tablero
+        /*
+         * Propiedad que representa la columna del tablero
+         */
         public Letra columna { get; init; }
 
         /*
@@ -68,52 +80,104 @@ namespace Recursos
 
     }
 
+    /*
+     * Clase Casilla que modela una casilla o escaque del tablero
+     */
     public class Casilla
     {
+        /*
+         * Propiedad Tono que indica de que color es la casilla
+         * Solo puede asignarse durante la construccion del objeto
+         */
         public Color Tono { get; init; }
+
+        /*
+         * Propiedad Coordenadas que indica que coordenadas se le asignan a esta casilla
+         * Solo puede asignarse durante la construccion del objeto
+         */
         public Coordenada Coordenadas { get; init; }
 
-        public Casilla(Color color, Coordenada coor)
+        /*
+         * Constructor parametrizado simple de una Casilla con tono y coordenadas
+         */
+        public Casilla(Color tono, Coordenada coor)
         {
-            Tono = color;
+            Tono = tono;
             Coordenadas = coor;
         }
     }
 
+    /*
+     * Clase Tablero que modela un tablero de ajedrez
+     */
     public class Tablero
     {
+        // Campo privado donde se almacena la matriz que representa el tablero
         private Casilla[,] matriz;
+
+        /*
+         * Propiedad Escaques que regula el acceso a la matriz representante del tablero
+         */
         public Casilla[,] Escaques
         {
             get => matriz;
         }
 
+        /*
+         * Constructor por defecto que define una representacion de un Tablero convencional de ajedrez
+         * (Tablero 8x8 con colores alternados)
+         */
         public Tablero()
         {
+            // Bandera que indicara que color asignarle a cada casilla
             bool turno = false;
 
+            // Definimos una matriz 8x8
             matriz = new Casilla[8,8];
+
+            // Iniciamos un bucle para recorrer la matriz
             for(int i=0; i < 8 ;i++){
                 for(int j=0; j < 8 ;j++){
+
+                    // Definimos el elemento de la Enumeracion color por asignar
                     Color color = turno ? Color.Blanco : Color.Negro;
+
+                    // Creamos una nueva casilla en el matriz[i,j]
                     matriz[i,j] = new Casilla(color, new Coordenada( i, (Coordenada.Letra)j ));
+
+                    // Alternamos el color para la siguiente casilla
                     turno = !turno;
+
                 }
+
+                // Alternamos el color con el que empieza la siguiente fila de casillas
                 turno = !turno;
             }
         }
     }
 
+    /*
+     * Clase Programa para pruebas
+     */
     public class Programa
     {
+
+        /*
+         * Metodo Main utilizado unicamente para probar y poder corregir errores con las clases que desarrollemos
+         */
         public static void Main(string[] args)
         {
+
+            /*
+             * En esta seccion revise la correcta construccion de un objeto de la clase Tablero
+             */
             Tablero mesa = new Tablero();
             for(int i=0; i<8 ; i++){
                 for(int j=0; j<8 ;j++){
                     Console.WriteLine(mesa.Escaques[i,j].Coordenadas.ToString() + " " + $"{mesa.Escaques[i,j].Tono}");
                 }
             }
+
         }
     }
 
