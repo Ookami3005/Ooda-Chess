f4619e63c4c793c13c9a796e8289a1e420635cbf
dannaliz <marquezdanna@ciencias.unam.mx>
2024-07-19 21:04:15 -0600

    piezas y toString

--- Recursos/Piezas.cs
+++ Recursos/Piezas.cs
@@ -14,17 +14,37 @@ namespace Recursos
 
         public abstract void posiblesMovimientos();
 
+        public abstract void toString();
     }
 
+    public class Peon : Pieza{
+    }
+
+    public class Caballo : Pieza{
+    }
+
+    public class Alfil : Pieza{
+    }
+
+    public class Torre : Pieza{
+    }
+
+    public class Reina : Pieza{
+    }
+
+    public class Rey : Pieza{
+    }
+    
     // Clase Peon (Ookami)
     // Clase Caballo (Ookami)
     // Clase Alfil (Amorcito)
     // Clase Torre (Cuchurrumin)
-    // Clase Dama (Ookami)
+    // Clase Dama (Ookami) //Quien le dice dama mi todo naco?, es la Reina
     // Clase Rey (Pastelito)
 }
 
 // Bug (No tocar)
+//Como que bug?
 namespace System.Runtime.CompilerServices
 {
     internal static class IsExternalInit {}
