2001fb48c0279246334d3b40385699688182ddfd
Fernando Romero <romerocruzfernando@gmail.com>
2024-07-22 18:35:06 -0600

    Oo: Implementado ejemplo muy basico de la clase Peon | Metodo MuestraCasilla

--- Recursos/Piezas.cs
+++ Recursos/Piezas.cs
@@ -1,38 +1,106 @@
 using System.Collections.Generic;
 
+/*
+ * ******************************* OODA CHESS **************************************
+ *
+ * Autores:
+ * - dannaliz
+ * - Ookami5018
+ *
+ * En este archivo definimos una parte de los recursos que utlizaremos para Ooda Chess
+ * Particularmente son aquellos que tienen que ver con las Piezas del juego
+ */
 namespace Recursos
 {
+
+    /*
+     * Enumeracion de los colores que pueden tener las piezas (y casillas)
+     */
     public enum Color
     {
         Blanco,
         Negro
     }
 
+    /*
+     * Clase abstracta Pieza que modela las caracteristicas basicas de una pieza del ajedrez
+     *
+     * NOTA: Amor, decidi quitarle la propiedad Coordenada y pasarsela como parametro al metodo
+     * Creo que es mas eficiente asi
+     */
     public abstract class Pieza
     {
 
-        public Color bando { get; init; }
-        public Casilla posicion { get; set; }
+        /*
+         * Propiedad que regula el acceso al Color de la pieza
+         */
+        public Color Bando { get; init; }
 
-        public abstract List<Casilla> PosiblesMovimientos();
+        /*
+         * Metodo abstracto que deja abierta la implementacion de los movimientos de la pieza
+         */
+        public abstract List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero);
 
+        /*
+         * Metodo abstracto sobreescrito ToString que deja abierta la implementacion de la cadena de texto
+         * que representa a cada pieza. Se hizo abstracto de manera que sea obligatorio implementarlo para clases derivadas
+         */
         public override abstract string ToString();
+
     }
 
+    /*
+     * Clase Peon derivada de Pieza que modela las caracteristicas de un Peon del ajedrez
+     *
+     * NOTA!!! Me falta implementar el ataque diagonal del peon
+     * Ademas falta pensar como implementar el comer al paso
+     */
     public class Peon : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+
+        /*
+         * Metodo sobreescrito que devuelve los posibles movimientos de un peon respecto a una casilla actual
+         *
+         * NOTA:
+         * Esta implementado el avance de una casilla del peon y el avance de 2 casillas en caso de estar
+         * en segunda fila
+         */
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
-            return null;
+            List<Casilla> lista = new List<Casilla>();
+            int filaActual = actual.Coordenadas.Fila;
+            bool esBlanco = this.Bando == Color.Blanco;
+            int segundaFila = esBlanco ? 1 : 6, ultimaFila = esBlanco ? 7 : 0;
+            int filaSiguiente = esBlanco ? filaActual+1 : filaActual-1;
+            if(filaActual != ultimaFila){
+                lista.Add(tablero.MuestraCasilla(filaSiguiente,actual.Coordenadas.Columna));
+            }
+            if(filaActual == segundaFila){
+                lista.Add(tablero.MuestraCasilla((filaSiguiente+1),actual.Coordenadas.Columna));
+            }
+            return lista;
         }
 
-        public override string ToString()
-        {
-            return "";
+        /*
+         * Devuelve el caracter Unicode de un peon del color correspondiente
+         *
+         * NOTA: Devuelvo el blanco cuando me piden el negro y viceversa
+         * Es que el peon blanco es "transparente" entonces por el fondo de la terminal se ve negro
+         * Y el negro es de color solido entonces es completamente blanco
+         * XD
+         */
+        public override string ToString() => this.Bando != Color.Blanco ? "\u265F" : "\u2659";
+
+        /*
+         * COnstructor parametrizado simple de un Peon, definiendo su color
+         */
+        public Peon(Color color){
+            this.Bando = color;
         }
+
     }
 
     public class Caballo : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
             return null;
 
@@ -44,8 +112,8 @@ namespace Recursos
         }
     }
 
-    public class Reina : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+    public class Dama : Pieza{
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
             return null;
 
@@ -58,7 +126,7 @@ namespace Recursos
     }
 
     public class Torre : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
             return null;
 
@@ -71,7 +139,7 @@ namespace Recursos
     }
 
     public class Rey : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
             return null;
 
@@ -84,7 +152,7 @@ namespace Recursos
     }
 
     public class Alfil : Pieza{
-        public  override List<Casilla> PosiblesMovimientos()
+        public  override List<Casilla> PosiblesMovimientos(Casilla actual, Tablero tablero)
         {
             return null;
 
